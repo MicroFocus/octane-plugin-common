e9aab50c4ce8f9cedce60a530837e75e761dca8c
Alexandra Marinescu <alexandra.marinescu@hpe.com>
2017-02-22 10:54:39 +0200

    initial commit for the service component that will be used by IntelliJ and Eclipse plugins

---
+++ src/test/java/com/hpe/adm/octane/DependencyInjectionITCase.java
@@ -0,0 +1,34 @@
+package com.hpe.adm.octane;
+
+import com.google.inject.Inject;
+import com.hpe.adm.octane.integrationtests.IntegrationTestBase;
+import com.hpe.adm.octane.integrationtests.util.ConfigurationUtil;
+import com.hpe.adm.octane.services.TestService;
+import com.hpe.adm.octane.services.connection.ConnectionSettingsProvider;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+/**
+ * This is just an example for now
+ */
+public class DependencyInjectionITCase extends IntegrationTestBase {
+
+    @Inject
+    private ConnectionSettingsProvider connectionSettingsProvider;
+
+    @Inject
+    private TestService testService;
+
+    @Test
+    public void test(){
+        //Test DI
+        assertNotNull(connectionSettingsProvider);
+        assertNotNull(testService);
+
+        //This should is loaded in the TestModule from the config file
+        assertEquals(connectionSettingsProvider.getConnectionSettings().getUserName(), ConfigurationUtil.getString(ConfigurationUtil.PropertyKeys.USERNAME));
+    }
+
+}
