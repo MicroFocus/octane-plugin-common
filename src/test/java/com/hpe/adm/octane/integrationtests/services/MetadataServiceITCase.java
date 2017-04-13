2bb2b19d4221aced9c9b1ffa823261649c021a48
osavencu <octavian.savencu@gmail.com>
2017-04-13 14:29:29 +0300

    tests for form retrieval

--- src/test/java/com/hpe/adm/octane/integrationtests/services/MetadataServiceITCase.java
+++ src/test/java/com/hpe/adm/octane/integrationtests/services/MetadataServiceITCase.java
@@ -3,6 +3,7 @@ package com.hpe.adm.octane.integrationtests.services;
 import com.google.inject.Inject;
 import com.hpe.adm.octane.integrationtests.IntegrationTestBase;
 import com.hpe.adm.octane.services.MetadataService;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import java.io.UnsupportedEncodingException;
@@ -13,7 +14,9 @@ public class MetadataServiceITCase extends IntegrationTestBase {
     MetadataService metadataService;
 
     @Test
+    @Ignore
     public void testGetAllFormLayoutsForEntityTypes() {
+        //TODO: osavencu: make it useful or delete it
         try {
             metadataService.getFormLayoutForAllEntityTypes();
         } catch (UnsupportedEncodingException e) {
