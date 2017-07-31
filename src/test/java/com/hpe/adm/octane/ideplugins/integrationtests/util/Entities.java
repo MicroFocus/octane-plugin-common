ccc34b9f85d93bec72fcfd7f484d0c2f4f294ed8
Kadar Tibor Manasse <kadar-tibor.manasse@hpe.com>
2017-07-31 11:28:45 +0300

    Created entities annotation

---
+++ src/test/java/com/hpe/adm/octane/ideplugins/integrationtests/util/Entities.java
@@ -0,0 +1,15 @@
+package com.hpe.adm.octane.ideplugins.integrationtests.util;
+
+import com.hpe.adm.octane.ideplugins.services.filtering.Entity;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface Entities {
+
+    Entity create();
+}
