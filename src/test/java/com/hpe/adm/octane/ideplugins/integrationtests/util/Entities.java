d31db2791ef85d3c47e50e15d22634aaa10f0c67
Kadar Tibor Manasse <kadar-tibor.manasse@hpe.com>
2017-07-31 11:36:18 +0300

    Entities annotation accepts now an array of entities

--- src/test/java/com/hpe/adm/octane/ideplugins/integrationtests/util/Entities.java
+++ src/test/java/com/hpe/adm/octane/ideplugins/integrationtests/util/Entities.java
@@ -6,10 +6,11 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+import java.util.List;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 public @interface Entities {
 
-    Entity create();
+    Entity[] requiredEntities();
 }
