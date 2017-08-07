4769834fb5a2a0c049eec299dc47e105772cfc2e
Kadar Tibor Manasse <kadar-tibor.manasse@hpe.com>
2017-08-07 12:21:39 +0300

    added test methods to check the sharedspace and workspace ids

--- src/test/java/com/hpe/adm/octane/ideplugins/unittests/ConnectionSettingsUTCase.java
+++ src/test/java/com/hpe/adm/octane/ideplugins/unittests/ConnectionSettingsUTCase.java
@@ -4,9 +4,13 @@ import com.hpe.adm.nga.sdk.authentication.SimpleUserAuthentication;
 import com.hpe.adm.nga.sdk.exception.OctaneException;
 import com.hpe.adm.nga.sdk.model.EntityModel;
 import com.hpe.adm.nga.sdk.network.OctaneHttpClient;
+import com.hpe.adm.nga.sdk.network.OctaneHttpRequest;
+import com.hpe.adm.nga.sdk.network.OctaneHttpResponse;
 import com.hpe.adm.nga.sdk.network.google.GoogleHttpClient;
 import com.hpe.adm.octane.ideplugins.integrationtests.IntegrationTestBase;
+import com.hpe.adm.octane.ideplugins.services.TestService;
 import com.hpe.adm.octane.ideplugins.services.connection.ConnectionSettings;
+import com.hpe.adm.octane.ideplugins.services.filtering.Entity;
 import com.hpe.adm.octane.ideplugins.services.util.ClientType;
 import com.hpe.adm.octane.ideplugins.services.util.SdkUtil;
 import org.apache.logging.log4j.LogManager;
@@ -25,10 +29,10 @@ public class ConnectionSettingsUTCase extends IntegrationTestBase {
         OctaneHttpClient octaneHttpClient = new GoogleHttpClient(connectionSettings.getBaseUrl());
         EntityModel entityModel = getCurrentUser();
         try {
-            assert octaneHttpClient.authenticate(new SimpleUserAuthentication(entityModel.getValue("email").getValue().toString(),"Welcome1",ClientType.HPE_MQM_UI.name()));
+            assert octaneHttpClient.authenticate(new SimpleUserAuthentication(entityModel.getValue("email").getValue().toString(), "Welcome1", ClientType.HPE_MQM_UI.name()));
         } catch (OctaneException e) {
-                String errorMessage = SdkUtil.getMessageFromOctaneException(e);
-                logger.error(errorMessage);
+            String errorMessage = SdkUtil.getMessageFromOctaneException(e);
+            logger.error(errorMessage);
         }
     }
 
@@ -39,7 +43,7 @@ public class ConnectionSettingsUTCase extends IntegrationTestBase {
         String username = getCurrentUser().getValue("email").getValue().toString();
         String password = UUID.randomUUID().toString();
         try {
-            assert !octaneHttpClient.authenticate(new SimpleUserAuthentication(username,password,ClientType.HPE_MQM_UI.name()));
+            assert !octaneHttpClient.authenticate(new SimpleUserAuthentication(username, password, ClientType.HPE_MQM_UI.name()));
 
         } catch (OctaneException e) {
             String errorMessage = SdkUtil.getMessageFromOctaneException(e);
@@ -54,7 +58,7 @@ public class ConnectionSettingsUTCase extends IntegrationTestBase {
         String username = UUID.randomUUID().toString();
         String password = "Welcome1"; //--default password
         try {
-            assert !octaneHttpClient.authenticate(new SimpleUserAuthentication(username,password,ClientType.HPE_MQM_UI.name()));
+            assert !octaneHttpClient.authenticate(new SimpleUserAuthentication(username, password, ClientType.HPE_MQM_UI.name()));
 
         } catch (OctaneException e) {
             String errorMessage = SdkUtil.getMessageFromOctaneException(e);
@@ -69,10 +73,70 @@ public class ConnectionSettingsUTCase extends IntegrationTestBase {
         String dummyUser = UUID.randomUUID().toString();
         String dummyPassword = UUID.randomUUID().toString();
         try {
-            assert !octaneHttpClient.authenticate(new SimpleUserAuthentication(dummyUser,dummyPassword,ClientType.HPE_MQM_UI.name()));
+            assert !octaneHttpClient.authenticate(new SimpleUserAuthentication(dummyUser, dummyPassword, ClientType.HPE_MQM_UI.name()));
         } catch (OctaneException e) {
             String errorMessage = SdkUtil.getMessageFromOctaneException(e);
             logger.error(errorMessage);
         }
     }
+
+
+    @Test
+    public void validateGoodWorkspaceGoodSharedSpace() {
+        TestService testService = new TestService();
+        ConnectionSettings connectionSettings = connectionSettingsProvider.getConnectionSettings();
+        try {
+            testService.getOctane(connectionSettings).entityList(Entity.WORK_ITEM_ROOT.getApiEntityName()).get().execute();
+            assert true;
+        } catch (OctaneException e) {
+            String message = SdkUtil.getMessageFromOctaneException(e);
+            logger.error(message);
+            assert false;
+        }
+    }
+
+    @Test
+    public void validateGoodWorkspaceWrongSharedSpace() {
+        TestService testService = new TestService();
+        ConnectionSettings connectionSettings = connectionSettingsProvider.getConnectionSettings();
+        connectionSettings.setSharedSpaceId(5000l);//--wrong shared space id, doesn't exist
+        try {
+            testService.getOctane(connectionSettings).entityList(Entity.WORK_ITEM_ROOT.getApiEntityName()).get().execute();
+        } catch (OctaneException e) {
+            String message = SdkUtil.getMessageFromOctaneException(e);
+            logger.error(message);
+            assert message.equals("The sharedspace or the workspace is incorrect.");
+        }
+    }
+
+    @Test
+    public void validateWrongWorkspaceGoodSharedSpace() {
+        TestService testService = new TestService();
+        ConnectionSettings connectionSettings = connectionSettingsProvider.getConnectionSettings();
+        connectionSettings.setWorkspaceId(5000l);//--wrong workspace id, doesn't exist
+        try {
+            testService.getOctane(connectionSettings).entityList(Entity.WORK_ITEM_ROOT.getApiEntityName()).get().execute();
+        } catch (OctaneException e) {
+            String message = SdkUtil.getMessageFromOctaneException(e);
+            logger.error(message);
+            assert message.equals("The sharedspace or the workspace is incorrect.");
+        }
+    }
+
+    @Test
+    public void validateWrongWorkspaceWrongSharedSpace() {
+        TestService testService = new TestService();
+        ConnectionSettings connectionSettings = connectionSettingsProvider.getConnectionSettings();
+        connectionSettings.setWorkspaceId(5000l);//--wrong workspace id, doesn't exist
+        connectionSettings.setSharedSpaceId(5001l);//--wrong sharedspace id, doesn't exist
+        try {
+            testService.getOctane(connectionSettings).entityList(Entity.WORK_ITEM_ROOT.getApiEntityName()).get().execute();
+        } catch (OctaneException e) {
+            String message = SdkUtil.getMessageFromOctaneException(e);
+            logger.error(message);
+            assert message.equals("The sharedspace or the workspace is incorrect.");
+        }
+    }
 }
+
+
