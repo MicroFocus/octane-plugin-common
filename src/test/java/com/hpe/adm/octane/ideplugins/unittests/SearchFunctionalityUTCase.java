8172b004a997bfed78d7d6fe8399f57030454505
Kadar Tibor Manasse <kadar-tibor.manasse@hpe.com>
2017-08-08 13:30:15 +0300

    added test class to test search functionality

---
+++ src/test/java/com/hpe/adm/octane/ideplugins/unittests/SearchFunctionalityUTCase.java
@@ -0,0 +1,86 @@
+package com.hpe.adm.octane.ideplugins.unittests;
+
+
+import com.google.inject.Inject;
+import com.hpe.adm.nga.sdk.model.EntityModel;
+import com.hpe.adm.nga.sdk.model.ReferenceFieldModel;
+import com.hpe.adm.octane.ideplugins.integrationtests.IntegrationTestBase;
+import com.hpe.adm.octane.ideplugins.services.filtering.Entity;
+import com.hpe.adm.octane.ideplugins.services.nonentity.EntitySearchService;
+import org.junit.Test;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+public class SearchFunctionalityUTCase extends IntegrationTestBase {
+    @Inject
+    private EntitySearchService searchService;
+
+
+
+    private EntityModel testSearch(String query) {
+        return searchService.searchGlobal(query, 1, Entity.USER_STORY).stream().collect(Collectors.toList()).iterator().next();
+    }
+
+    private List<EntityModel> createSearchableEntities() {
+        List<EntityModel> entities = new ArrayList<>();
+        entities.add(createEntity(Entity.DEFECT));
+        entities.add(createEntity(Entity.MANUAL_TEST));
+        entities.add(createEntity(Entity.GHERKIN_TEST));
+        EntityModel userStoryWithTask = createEntity(Entity.USER_STORY);
+        entities.add(userStoryWithTask);
+        entities.add(createTask(userStoryWithTask, "task 1" + UUID.randomUUID()));
+        return entities;
+    }
+
+    private void setDescription(List<EntityModel> entities){
+        int descriptionCount = 0;
+        for(EntityModel entityModel:entities){
+            setDescription(entityModel,String.valueOf(descriptionCount++));
+        }
+    }
+
+    private boolean compareEntities(EntityModel entity1, EntityModel entity2){
+        ReferenceFieldModel subField = null;
+        String entityType = entity2.getValue("entity_type").getValue().toString();
+        if (entityType.equals("work_item")) {
+            subField = (ReferenceFieldModel) entity2.getValue("my_follow_items_work_item");
+        }
+        if (entityType.equals("test")) {
+            subField = (ReferenceFieldModel) entity2.getValue("my_follow_items_test");
+        }
+        if (entityType.equals("task")) {
+            subField = (ReferenceFieldModel) entity2.getValue("my_follow_items_task");
+        }
+
+        if (subField.getValue().getValue("id").getValue().toString().equals(entity1.getValue("id").getValue().toString())) {
+            return true;
+        }
+        return false;
+    }
+
+    @Test
+    public void testSearchEntities(){
+        List<EntityModel> entityModels = createSearchableEntities();
+
+        setDescription(entityModels);
+
+        int descriptionCount = 0;
+
+        for(EntityModel entityModel:entityModels){
+            //search by name
+            EntityModel retrievedEntity =  testSearch(entityModel.getValue("name").getValue().toString());
+            assert compareEntities(entityModel,retrievedEntity);
+            //search by id
+            retrievedEntity = testSearch(entityModel.getValue("id").getValue().toString());
+            assert compareEntities(entityModel,retrievedEntity);
+            //search by description
+            retrievedEntity = testSearch(entityModel.getValue("description").getValue().toString());
+            assert compareEntities(entityModel,retrievedEntity);
+        }
+    }
+
+
+
+}
