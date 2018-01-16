7c10fa0595eef634c5865b252323d1189215a07e
Kadar Tibor Manasse <kadar-tibor.manasse@hpe.com>
2018-01-16 14:53:38 +0200

    factored out constants

--- src/test/java/com/hpe/adm/octane/ideplugins/Constants.java
+++ src/test/java/com/hpe/adm/octane/ideplugins/Constants.java
@@ -34,64 +34,64 @@ public class Constants {
     public static final String WORK_ITEM_ROOT = "work_item_root";
 
     public interface ManualRun {
-        public static final String RUN = "run";
-        public static final String NAME = "test_manual_run";
-        public static final String SUBTYPE = "run_manual";
+        String RUN = "run";
+        String NAME = "test_manual_run";
+        String SUBTYPE = "run_manual";
     }
 
 
     public interface Requirement {
-        public static final String ID = "phase.requirement_document.draft";
-        public static final String NAME = "Draft";
-        public static final String LOGICAL_NAME = "phase.requirement_document.draft";
-        public static final String TYPE = "requirement";
-        public static final String DOCUMENT = "requirement_document";
-        public static final String FOLDER = "requirement_folder";
+        String ID = "phase.requirement_document.draft";
+        String NAME = "Draft";
+        String LOGICAL_NAME = "phase.requirement_document.draft";
+        String TYPE = "requirement";
+        String DOCUMENT = "requirement_document";
+        String FOLDER = "requirement_folder";
     }
 
     public interface Workspace {
-        public static final String NAME_VALUE = "test_workspace1";
-        public static final String DESCRIPTION = "Created from intellij";
-        public static final String WORKSPACE_ID = "workspace_id";
+        String NAME_VALUE = "test_workspace1";
+        String DESCRIPTION = "Created from intellij";
+        String WORKSPACE_ID = "workspace_id";
     }
 
     public interface Errors {
-        public static final String CONNECTION_SETTINGS_RETRIEVE_ERROR = "Cannot retrieve connection settings from either vm args or prop file, cannot run tests";
+        String CONNECTION_SETTINGS_RETRIEVE_ERROR = "Cannot retrieve connection settings from either vm args or prop file, cannot run tests";
     }
 
     public interface NativeStatus {
-        public static final String NATIVE_STATUS_TYPE_VALUE = "list_node";
-        public static final String NATIVE_STATUS_NEW_ID = "1094";
-        public static final String NATIVE_STATUS_OLD_ID = "1091";
+        String NATIVE_STATUS_TYPE_VALUE = "list_node";
+        String NATIVE_STATUS_NEW_ID = "1094";
+        String NATIVE_STATUS_OLD_ID = "1091";
     }
 
     public interface User {
-        public static final String FULL_NAME = "full_name";
-        public static final String LAST_NAME = "last_name";
-        public static final String USER_TYPE = "workspace_user";
-        public static final String FIRST_NAME = "first_name";
-        public static final String EMAIL = "email";
-        public static final String EMAIL_DOMAIN = "@hpe.com";
-        public static final String PASSWORD = "password";
-        public static final String PASSWORD_VALUE = "Welcome1";
-        public static final String PHONE = "phone1";
-        public static final String PHONE_NR = "0875432135";
-        public static final String USER_ROLES = "user_roles";
+        String FULL_NAME = "full_name";
+        String LAST_NAME = "last_name";
+        String USER_TYPE = "workspace_user";
+        String FIRST_NAME = "first_name";
+        String EMAIL = "email";
+        String EMAIL_DOMAIN = "@hpe.com";
+        String PASSWORD = "password";
+        String PASSWORD_VALUE = "Welcome1";
+        String PHONE = "phone1";
+        String PHONE_NR = "0875432135";
+        String USER_ROLES = "user_roles";
     }
 
     public interface Release {
-        public static final String RELEASES = "releases";
-        public static final String TYPE = "release";
-        public static final String NAME = "test_Release";
-        public static final String START_DATE = "start_date";
-        public static final String END_DATE = "end_date";
+        String RELEASES = "releases";
+        String TYPE = "release";
+        String NAME = "test_Release";
+        String START_DATE = "start_date";
+        String END_DATE = "end_date";
     }
 
     public interface AgileType {
-        public static final String AGILE_TYPE = "agile_type";
-        public static final String NEW_ID = "list_node.release_agile_type.scrum";
-        public static final String OLD_ID = "1108";
-        public static final String NAME = "scrum";
-        public static final String TYPE = "list_node";
+        String AGILE_TYPE = "agile_type";
+        String NEW_ID = "list_node.release_agile_type.scrum";
+        String OLD_ID = "1108";
+        String NAME = "scrum";
+        String TYPE = "list_node";
     }
 }
