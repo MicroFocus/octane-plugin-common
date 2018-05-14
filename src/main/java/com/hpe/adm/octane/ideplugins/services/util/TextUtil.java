d57ec9713fc8da46b910f7544838b5e34edb0eee
vladbenchea <vlad.benchea@hpe.com>
2018-05-14 11:51:54 +0300

    Defect #612022: Fixed bug for when searching for empty string, simple and double quotes and backslash

--- src/main/java/com/hpe/adm/octane/ideplugins/services/util/TextUtil.java
+++ src/main/java/com/hpe/adm/octane/ideplugins/services/util/TextUtil.java
@@ -16,8 +16,7 @@ package com.hpe.adm.octane.ideplugins.services.util;
 public class TextUtil {
 
     private enum EscapeEnum {
-        SIMPLE_QUOTE("\'", "\\\'"),
-        DOUBLE_QUOTE("\"", "\\\""),
+        DOUBLE_QUOTE("\"", "\\\""), 
         BACKSLASH("\\", "\\\\");
 
         private String wrong;
@@ -41,22 +40,30 @@ public class TextUtil {
     }
 
     /**
-     * This method was called to escape special characters from a text (e.g. when searching for text having \ or ` will make the REST call crash) <br>
-     *      - a split on the text was required because the query failed mostly when the special character was isolated (e.g. " texttext text {specialChar} text text") <br>
-     *      - an inner enum class was used for this method as it is only used for char escaping; it was not designed for public usage. <br>
+     * This method was called to escape special characters from a text (e.g.
+     * when searching for text having \ or ` will make the REST call crash) <br>
+     * - a split on the text was required because the query failed mostly when
+     * the special character was isolated (e.g. " texttext text {specialChar}
+     * text text") <br>
+     * - an inner enum class was used for this method as it is only used for
+     * char escaping; it was not designed for public usage. <br>
      *
-     * @param text input text to be escaped
+     * @param text
+     *            input text to be escaped
      * @return String text with escaped special characters
      */
     public static String escapeText(String text) {
         String result = text;
+        String[] textMembers = text.split("");
+        boolean flagForDoubleQuote = false;
 
-        String[] textMembers = text.split(" ");
-
-        for (final String member : textMembers) {
+        for (String member : textMembers) {
             for (EscapeEnum escapeEnum : EscapeEnum.values()) {
-                if (member.equals(escapeEnum.getWrong())) {
+                if (member.equals(escapeEnum.getWrong()) && !flagForDoubleQuote) {
                     result = result.replace(member, escapeEnum.getCorrect());
+                    if (member.equals("\"")) {
+                        flagForDoubleQuote = true;
+                    }
                     break;
                 }
             }
