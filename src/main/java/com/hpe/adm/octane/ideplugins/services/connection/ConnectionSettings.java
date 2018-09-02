69a220781adfd6b94da8ca3a204dd22e6509581f
Andras Toth <atoth@hpe.com>
2018-09-02 19:39:39 +0300

    user story #693430: Fixed equals method for ConnectionSettings object

--- src/main/java/com/hpe/adm/octane/ideplugins/services/connection/ConnectionSettings.java
+++ src/main/java/com/hpe/adm/octane/ideplugins/services/connection/ConnectionSettings.java
@@ -13,9 +13,10 @@
 
 package com.hpe.adm.octane.ideplugins.services.connection;
 
-import com.hpe.adm.nga.sdk.authentication.Authentication;
-
 import java.util.Objects;
+
+import com.hpe.adm.nga.sdk.authentication.Authentication;
+import com.hpe.adm.octane.ideplugins.services.connection.sso.SsoAuthentication;
 
 public class ConnectionSettings {
 
@@ -66,26 +67,36 @@ public class ConnectionSettings {
         this.authentication = authentication;
     }
 
-    public boolean isEmpty(){
+    public boolean isEmpty() {
         return isStringEmpty(baseUrl) &&
-                sharedSpaceId==null &&
-                workspaceId==null &&
+                sharedSpaceId == null &&
+                workspaceId == null &&
                 authentication == null;
     }
 
-    private boolean isStringEmpty(String str){
+    private boolean isStringEmpty(String str) {
         return str == null || str.trim().length() == 0;
     }
 
+    /**
+     * Settings which both have {@link SsoAuthentication} will never be equal,
+     * since the crendentials cannot be predetermined
+     */
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
         ConnectionSettings that = (ConnectionSettings) o;
+
         return Objects.equals(baseUrl, that.baseUrl) &&
                 Objects.equals(sharedSpaceId, that.sharedSpaceId) &&
                 Objects.equals(workspaceId, that.workspaceId) &&
-                Objects.equals(authentication.getClass(), that.authentication.getClass());
+                Objects.equals(authentication.getClass(), that.authentication.getClass()) &&
+                Objects.equals(authentication.getAuthenticationString(), that.authentication.getAuthenticationString()) &&
+                !authentication.getClass().equals(SsoAuthentication.class);
+
     }
 
     @Override
@@ -95,14 +106,17 @@ public class ConnectionSettings {
 
     /**
      * Set the internal state of the object to math the param
-     * @param connectionSettings {@link ConnectionSettings} to be copied
+     * 
+     * @param connectionSettings
+     *            {@link ConnectionSettings} to be copied
      * @return copy of param
      */
-    public static ConnectionSettings getCopy(ConnectionSettings connectionSettings){
+    public static ConnectionSettings getCopy(ConnectionSettings connectionSettings) {
         return new ConnectionSettings(
                 connectionSettings.getBaseUrl(),
                 connectionSettings.getSharedSpaceId(),
                 connectionSettings.getWorkspaceId(),
-                connectionSettings.getAuthentication()); //TODO: BAD BAD DOES NOT COPY
+                connectionSettings.getAuthentication()); // TODO: BAD BAD DOES
+                                                         // NOT COPY
     }
 }
