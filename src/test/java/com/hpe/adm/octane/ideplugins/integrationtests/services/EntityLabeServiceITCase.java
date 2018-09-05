de2582bc1b87dcdf543df4f31bc4020f12dbd283
kdrtibor <kadar-tibor.manasse@hpe.com>
2018-09-05 11:57:28 +0300

    user story #664174: updates the test for entity labels

--- src/test/java/com/hpe/adm/octane/ideplugins/integrationtests/services/EntityLabeServiceITCase.java
+++ src/test/java/com/hpe/adm/octane/ideplugins/integrationtests/services/EntityLabeServiceITCase.java
@@ -4,18 +4,43 @@ import com.google.inject.Inject;
 import com.hpe.adm.nga.sdk.model.EntityModel;
 import com.hpe.adm.octane.ideplugins.integrationtests.IntegrationTestBase;
 import com.hpe.adm.octane.ideplugins.services.EntityLabelService;
+import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.Arrays;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class EntityLabeServiceITCase extends IntegrationTestBase {
 
     @Inject
     private EntityLabelService entityLabelService;
 
+    private String[] entityTypes = new String[]{"defect",
+            "story",
+            "quality_story",
+            "feature",
+            "epic",
+            "task",
+            "test_manual",
+            "gherkin_test",
+            "test_suite",
+            "run_manual",
+            "run_suite",
+            "test_automated",
+            "comment",
+            "requirement"};
+
     @Test
     public void testGetEntityLabelDetails() {
         Map<String, EntityModel> entityLabelList = entityLabelService.getEntityLabelDetails();
-        System.out.println(entityLabelList.size());
+
+        AtomicBoolean areEntityTypesCovered = new AtomicBoolean(true);
+        Arrays.stream(entityTypes).forEach(e -> {
+            if(entityLabelList.get(e) == null)
+                areEntityTypesCovered.set(false);
+        });
+
+        Assert.assertTrue(areEntityTypesCovered.get());
     }
 }
