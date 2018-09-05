d5fcb09f11411497decb8f25d4bb9c605eff815b
kdrtibor <kadar-tibor.manasse@hpe.com>
2018-09-05 07:43:46 +0300

    user story #664174: uses the http client from the provider instead of creating its own - instead of creating a map with entity type and name, keep the relevant informations in entityModels

--- src/main/java/com/hpe/adm/octane/ideplugins/services/EntityLabelService.java
+++ src/main/java/com/hpe/adm/octane/ideplugins/services/EntityLabelService.java
@@ -2,6 +2,8 @@ package com.hpe.adm.octane.ideplugins.services;
 
 import com.google.inject.Inject;
 import com.hpe.adm.nga.sdk.authentication.SimpleUserAuthentication;
+import com.hpe.adm.nga.sdk.model.EntityModel;
+import com.hpe.adm.nga.sdk.model.StringFieldModel;
 import com.hpe.adm.nga.sdk.network.OctaneHttpClient;
 import com.hpe.adm.nga.sdk.network.OctaneHttpRequest;
 import com.hpe.adm.nga.sdk.network.OctaneHttpResponse;
@@ -13,8 +15,7 @@ import com.hpe.adm.octane.ideplugins.services.util.ClientType;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class EntityLabelService {
 
@@ -22,21 +23,16 @@ public class EntityLabelService {
     private HttpClientProvider httpClientProvider;
 
     @Inject
-    private OctaneProvider octaneProvider;
-
-    @Inject
     private ConnectionSettingsProvider connectionSettingsProvider;
 
-    public Map<String, String> getEntityLabelDetails(){
+    public Collection<EntityModel> getEntityLabelDetails(){
         String getUrl = connectionSettingsProvider.getConnectionSettings().getBaseUrl() + "/api/shared_spaces/" +
                 connectionSettingsProvider.getConnectionSettings().getSharedSpaceId() + "/workspaces/" +
                 connectionSettingsProvider.getConnectionSettings().getWorkspaceId() + "/entity_labels";
 
-        String urlDomain = connectionSettingsProvider.getConnectionSettings().getBaseUrl();
-
         OctaneHttpRequest getOctaneHttpRequest = new OctaneHttpRequest.GetOctaneHttpRequest(getUrl);
 
-        OctaneHttpClient octaneHttpClient = new GoogleHttpClient(urlDomain);
+        OctaneHttpClient octaneHttpClient = httpClientProvider.geOctaneHttpClient();
         octaneHttpClient.authenticate(new SimpleUserAuthentication(connectionSettingsProvider.getConnectionSettings().getUserName(),
                 connectionSettingsProvider.getConnectionSettings().getPassword(), ClientType.HPE_MQM_UI.name()));
         OctaneHttpResponse response = null;
@@ -47,24 +43,26 @@ public class EntityLabelService {
             System.out.println("Failed");
         }
 
-        return new HashMap<>(getEntityMapFromJSON(response.getContent()));
+        return getEntityMapFromJSON(response.getContent());
     }
 
-    private Map<String, String> getEntityMapFromJSON(String jsonString){
-        Map<String, String> map = new HashMap<>();
+    private Collection<EntityModel> getEntityMapFromJSON(String jsonString){
+        List<EntityModel> entityLabelList = new ArrayList<>();
 
         JSONObject shellObject = new JSONObject(jsonString);
         JSONArray entityLabelJSONObjects = shellObject.getJSONArray("data");
         for(Object entityLabelObject : entityLabelJSONObjects){
             if(entityLabelObject instanceof JSONObject) {
                 if(((JSONObject) entityLabelObject).get("language").equals("lang.en")) {
-                    map.put(((JSONObject) entityLabelObject).getString("entity_type"),
-                            ((JSONObject) entityLabelObject).getString("name"));
+                    EntityModel em = new EntityModel();
+                    em.setValue(new StringFieldModel("entity_type", ((JSONObject) entityLabelObject).getString("entity_type")));
+                    em.setValue(new StringFieldModel("name", ((JSONObject) entityLabelObject).getString("name")));
+                    em.setValue((new StringFieldModel("initials", ((JSONObject) entityLabelObject).getString("initials"))));
+                    entityLabelList.add(em);
                 }
             }
         }
-
-        return map;
+        return entityLabelList;
     }
 
 
