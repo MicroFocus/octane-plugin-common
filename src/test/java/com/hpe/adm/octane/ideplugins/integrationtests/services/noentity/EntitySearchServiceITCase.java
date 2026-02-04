16ebf16adea09397e757033382560e892bef0892
SerbanIonescu <sionescu@opentext.com>
2026-02-04 15:08:29 +0200

    user_story #3020159: remove_CE_or_cloud_edition

--- src/test/java/com/hpe/adm/octane/ideplugins/integrationtests/services/noentity/EntitySearchServiceITCase.java
+++ src/test/java/com/hpe/adm/octane/ideplugins/integrationtests/services/noentity/EntitySearchServiceITCase.java
@@ -1,11 +1,11 @@
 /*******************************************************************************
- * Copyright 2017-2023 Open Text.
+ * Copyright 2017-2026 Open Text.
  *
  * The only warranties for products and services of Open Text and
  * its affiliates and licensors (“Open Text”) are as may be set forth
  * in the express warranty statements accompanying such products and services.
  * Nothing herein should be construed as constituting an additional warranty.
- * Open Text shall not be liable for technical or editorial errors or
+ * Open Text shall not be l302iable for technical or editorial errors or
  * omissions contained herein. The information contained herein is subject
  * to change without notice.
  *
